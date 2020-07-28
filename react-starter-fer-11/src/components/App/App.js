ba7e0b810e33ce8bfbd6242af106e456f9e4ca70
Magdalena Marchewka <madilenaa@gmail.com>
2020-07-28 20:36:46 +0200

    Add new components

--- react-starter-fer-11/src/components/App/App.js
+++ react-starter-fer-11/src/components/App/App.js
@@ -6,9 +6,11 @@ class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello World!</h2>
-        <List />
+        <h1 className={styles.title}>MY FIRST REACT APP</h1>
+        <h2 className={styles.subtitle}>A simple to-do app, with lists, columns and cards</h2>
+        <List title={['Things to do', <sup>soon!</sup>]} image={"/src/images/space.png"}>
+          <p>Im planning on doing all these things sooner, rather than later!</p>
+        </List>
       </main>
     )
   }
--- react-starter-fer-11/src/components/Hero/Hero.js
+++ react-starter-fer-11/src/components/Hero/Hero.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>Things to do</h2>
-    <img src="/src/images/space.png" className={styles.image}></img>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src={props.image}></img>
   </header>
 );
+Hero.propTypes = {
+titleText: PropTypes.node,
+};
 
 export default Hero;
