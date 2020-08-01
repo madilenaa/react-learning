40446cdf39ff7678e125bdd77e77c113331d2d13
Magdalena Marchewka <madilenaa@gmail.com>
2020-08-01 13:25:12 +0200

    modife components

--- react-starter-fer-11/src/components/App/App.js
+++ react-starter-fer-11/src/components/App/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>MY FIRST REACT APP</h1>
-        <h2 className={styles.subtitle}>A simple to-do app, with lists, columns and cards</h2>
-        <List title={['Things to do', <sup>soon!</sup>]} image={"/src/images/space.png"}>
-          <p>Im planning on doing all these things sooner, rather than later!</p>
-        </List>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <List {...listData} />
       </main>
     )
   }
